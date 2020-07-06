a23ddf679444e8f3d3762abb91bd7beb972a16d7
grace22411 <olayinkagrace112.ob@gmail.com>
2020-07-06 07:37:28 -0700

    fully done

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ $(document).ready(function(){
     $(".success").fadeOut();
   });
       
+
+  $(".fa-arrow-down").click(function(){
+		var $element = $(".what-we-offer").offset().top;
+	
+		$("html,body").animate({
+			scrollTop : $element
+    }, 900);
+  });
 });
